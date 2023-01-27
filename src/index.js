79cdc438d34d83d59efebd7f985f81d7553b1085
Viacheslav Plevako <cheryec@ukr.net>
2023-01-27 17:18:42 +0200

    new

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+const inputEl = document.querySelector('#search-box');
+let countryName = '';
 
-fetch('https://restcountries.com/v2/all?fields=name,capital,currencies').then(result => result.json()).then(console.log);
+inputEl.addEventListener('input', onInput);
 
+
+function onInput(e) {
+  countryName = e.currentTarget.value;
+  console.log(countryName);
+  return countryName;
+}
+
+
+fetch('https://restcountries.com/v3.1/all?fields=name,capital,population,flags,languages').then(result => result.json()).then(country => console.log(country));
